9a7d6f90c3367c846a5b8a52abd085b4178d2efe
lilchicky <thelilchicken01@gmail.com>
2025-04-26 03:14:46 -0600

    breaking suspicious logs with no silk touch spawns glimmerbug

--- src/main/java/com/gmail/thelilchicken01/ethermist/block/GlimmerbugBlocks.java
+++ src/main/java/com/gmail/thelilchicken01/ethermist/block/GlimmerbugBlocks.java
@@ -1,18 +1,61 @@
 package com.gmail.thelilchicken01.ethermist.block;
 
+import com.gmail.thelilchicken01.ethermist.entity.EMEntityTypes;
+import com.gmail.thelilchicken01.ethermist.entity.mobs.GlimmerbugEntity;
 import com.gmail.thelilchicken01.ethermist.particle.EMParticleTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class GlimmerbugBlocks extends EMRotatedPillarBlock {
 
     public GlimmerbugBlocks() {
         super(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_WOOD).lightLevel((state) -> 12), true);
+    }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+        if (!level.isClientSide() && !player.isCreative()) {
+
+            AtomicBoolean isSilkTouch = new AtomicBoolean(false);
+
+            EnchantmentHelper.runIterationOnItem(player.getMainHandItem(), (enchantHolder, enchantLevel) -> {
+                if (enchantHolder.is(Enchantments.SILK_TOUCH.location())) {
+                    isSilkTouch.set(true);
+                }
+            });
+
+            if (!isSilkTouch.get()) {
+                GlimmerbugEntity bug = new GlimmerbugEntity(EMEntityTypes.GLIMMERBUG.get(), level);
+
+                bug.setPos(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
+
+                bug.setYRot((float) (Math.random() * 360.0));
+                bug.setYHeadRot(bug.getYRot());
+
+                bug.finalizeSpawn(
+                        (ServerLevelAccessor) level,
+                        level.getCurrentDifficultyAt(pos),
+                        MobSpawnType.MOB_SUMMONED,
+                        null
+                );
+
+                level.addFreshEntity(bug);
+            }
+        }
+        return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
     }
 
     @Override
