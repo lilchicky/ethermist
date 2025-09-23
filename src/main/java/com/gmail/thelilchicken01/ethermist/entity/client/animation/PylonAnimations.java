522899b7f3cc4cd050be77e70892753b240c79c5
lilchicky <thelilchicken01@gmail.com>
2025-09-22 20:46:14 -0600

    slightly updated pylon animation

--- src/main/java/com/gmail/thelilchicken01/ethermist/entity/client/animation/PylonAnimations.java
+++ src/main/java/com/gmail/thelilchicken01/ethermist/entity/client/animation/PylonAnimations.java
@@ -10,11 +10,11 @@ public class PylonAnimations {
     public static final AnimationDefinition PYLON_IDLE = AnimationDefinition.Builder.withLength(2f).looping()
             .addAnimation("body",
                     new AnimationChannel(AnimationChannel.Targets.POSITION,
-                            new Keyframe(0f, KeyframeAnimations.posVec(0f, 0f, 0f),
+                            new Keyframe(0f, KeyframeAnimations.posVec(0f, -1f, 0f),
                                     AnimationChannel.Interpolations.LINEAR),
                             new Keyframe(1f, KeyframeAnimations.posVec(0f, 1f, 0f),
                                     AnimationChannel.Interpolations.CATMULLROM),
-                            new Keyframe(2f, KeyframeAnimations.posVec(0f, 0f, 0f),
+                            new Keyframe(2f, KeyframeAnimations.posVec(0f, -1f, 0f),
                                     AnimationChannel.Interpolations.LINEAR))).build();
 
 }
