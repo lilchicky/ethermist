b74115f3b0b11fc0d9abbf4948e55e24baf3472b
lilchicky <thelilchicken01@gmail.com>
2025-10-12 02:41:12 -0600

    fixed pylon animations

--- src/main/java/com/gmail/thelilchicken01/ethermist/entity/client/animation/PylonAnimations.java
+++ src/main/java/com/gmail/thelilchicken01/ethermist/entity/client/animation/PylonAnimations.java
@@ -7,14 +7,15 @@ import net.minecraft.client.animation.KeyframeAnimations;
 
 public class PylonAnimations {
 
-    public static final AnimationDefinition PYLON_IDLE = AnimationDefinition.Builder.withLength(2f).looping()
-            .addAnimation("body",
+
+    public static final AnimationDefinition IDLE = AnimationDefinition.Builder.withLength(2f).looping()
+            .addAnimation("main",
                     new AnimationChannel(AnimationChannel.Targets.POSITION,
                             new Keyframe(0f, KeyframeAnimations.posVec(0f, -1f, 0f),
-                                    AnimationChannel.Interpolations.LINEAR),
+                                    AnimationChannel.Interpolations.CATMULLROM),
                             new Keyframe(1f, KeyframeAnimations.posVec(0f, 1f, 0f),
                                     AnimationChannel.Interpolations.CATMULLROM),
                             new Keyframe(2f, KeyframeAnimations.posVec(0f, -1f, 0f),
-                                    AnimationChannel.Interpolations.LINEAR))).build();
+                                    AnimationChannel.Interpolations.CATMULLROM))).build();
 
 }
--- src/main/java/com/gmail/thelilchicken01/ethermist/entity/client/model/PylonModel.java
+++ src/main/java/com/gmail/thelilchicken01/ethermist/entity/client/model/PylonModel.java
@@ -17,35 +17,42 @@ import net.minecraft.resources.ResourceLocation;
 
 public class PylonModel<T extends PylonEntity> extends HierarchicalModel<T> {
 
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(Ethermist.MODID, "pylon"), "main");
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(
+			ResourceLocation.fromNamespaceAndPath(Ethermist.MODID, "pylon"), "main");
 	private final ModelPart body;
+	private final ModelPart main;
 
 	public PylonModel(ModelPart root) {
 		this.body = root.getChild("body");
+		this.main = this.body.getChild("main");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -23.0F, -3.0F, 8.0F, 12.0F, 6.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 25).addBox(-3.0F, -23.0F, -4.0F, 6.0F, 12.0F, 1.0F, new CubeDeformation(0.0F))
-		.texOffs(14, 25).addBox(-3.0F, -23.0F, 3.0F, 6.0F, 12.0F, 1.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 18).addBox(-3.0F, -11.0F, -3.0F, 6.0F, 1.0F, 6.0F, new CubeDeformation(0.0F))
-		.texOffs(24, 18).addBox(-3.0F, -24.0F, -3.0F, 6.0F, 1.0F, 6.0F, new CubeDeformation(0.0F))
-		.texOffs(28, 0).addBox(-2.0F, -10.0F, -2.0F, 4.0F, 2.0F, 4.0F, new CubeDeformation(0.0F))
-		.texOffs(28, 12).addBox(-1.0F, -8.0F, -1.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
-		.texOffs(28, 15).addBox(-1.0F, -27.0F, -1.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
-		.texOffs(28, 6).addBox(-2.0F, -26.0F, -2.0F, 4.0F, 2.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition main = body.addOrReplaceChild("main", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, 1.0F, -3.0F, 8.0F, 12.0F, 6.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 25).addBox(-3.0F, 1.0F, -4.0F, 6.0F, 12.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(14, 25).addBox(-3.0F, 1.0F, 3.0F, 6.0F, 12.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 18).addBox(-3.0F, 13.0F, -3.0F, 6.0F, 1.0F, 6.0F, new CubeDeformation(0.0F))
+				.texOffs(24, 18).addBox(-3.0F, 0.0F, -3.0F, 6.0F, 1.0F, 6.0F, new CubeDeformation(0.0F))
+				.texOffs(28, 0).addBox(-2.0F, 14.0F, -2.0F, 4.0F, 2.0F, 4.0F, new CubeDeformation(0.0F))
+				.texOffs(28, 12).addBox(-1.0F, 16.0F, -1.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(28, 15).addBox(-1.0F, -3.0F, -1.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(28, 6).addBox(-2.0F, -2.0F, -2.0F, 4.0F, 2.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -24.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
 
 	@Override
 	public void setupAnim(PylonEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
 		this.root().getAllParts().forEach(ModelPart::resetPose);
 
-		this.animate(entity.idleState, PylonAnimations.PYLON_IDLE, ageInTicks, 1f);
+		this.animate(entity.idleState, PylonAnimations.IDLE, ageInTicks, 1f);
+
 	}
 
 	@Override
